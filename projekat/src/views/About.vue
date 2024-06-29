09f5d3a839803fccfe79bc6947fd39b4b05b69a5
lazar <kl210633d@student.etf.bg.ac.rs>
2024-06-29 18:02:14 +0200

    footer popravljen

--- projekat/src/views/About.vue
+++ projekat/src/views/About.vue
@@ -2,10 +2,6 @@
   <div class="about">
     <h1>This is an about page</h1>
  
-  <footer> 
-      <div class="footer-bottom">
-      <p>  „Copyright 2024, Матија Милић/Лазар Крповић, Одсек за софтверско инжењерство Електротехничког факултета Универзитета у Београду“ </p>
-      </div>
-  </footer>
+
   </div>
 </template>
--- projekat/src/views/Home.vue
+++ projekat/src/views/Home.vue
@@ -6,47 +6,8 @@
     dugme
   </button>
   </router-link>
-  <footer> 
-      <div class="footer-bottom">
-      <p>  „Copyright 2024, Матија Милић/Лазар Крповић, Одсек за софтверско инжењерство Електротехничког факултета Универзитета у Београду“ </p>
-      </div>
-  </footer>
+ 
   </div>
-</template>
+  </template>
 
 
-<style>
-footer{
-  font-weight: 200;
-  background: #111;
-  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
-  color: #fff;
-  bottom: 0;
-  position: fixed
-}
-
-.footer-bottom{
-  width: 100vw;
-  padding: 3px 0;
-  text-align: center;
-  
-}
-.footer-bottom p{
-  font-size: 14;
-  word-spacing: 2px;
-  text-transform: capitalize;
-  background: linear-gradient(90deg, #f73100, #9b2424, #f73100);
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
-}
-</style>
-
-<script>
-// @ is an alias to /src
-
-
-export default {
-  name: 'Home',
-  
-}
-</script>
